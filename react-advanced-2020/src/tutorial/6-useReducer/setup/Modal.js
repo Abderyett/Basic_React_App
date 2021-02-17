55e95a683caaec5baaa6559269b2d432fe68b113
Abderaouf Yettou <abderaouf.yettou@gmail.com>
2021-02-17 22:29:50 +0100

    useReducer

--- react-advanced-2020/src/tutorial/6-useReducer/setup/Modal.js
+++ react-advanced-2020/src/tutorial/6-useReducer/setup/Modal.js
@@ -1,7 +1,22 @@
 import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
 
-const Modal = () => {
-  return <div>i'm modal</div>;
+const Modal = ({ modelContent, closeModal }) => {
+  useEffect(() => {
+    setTimeout(() => {
+      closeModal();
+    }, 3000);
+  });
+  return (
+    <div className="modal">
+      <p>{modelContent}</p>
+    </div>
+  );
+};
+
+Modal.propTypes = {
+  modelContent: PropTypes.string,
+  closeModal: PropTypes.func,
 };
 
 export default Modal;
--- react-advanced-2020/src/tutorial/6-useReducer/setup/index.js
+++ react-advanced-2020/src/tutorial/6-useReducer/setup/index.js
@@ -1,10 +1,80 @@
 import React, { useState, useReducer } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 import Modal from './Modal';
 import { data } from '../../../data';
 // reducer function
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'ADD':
+      return {
+        ...state,
+        people: [...state.people, action.payload],
+        showModal: true,
+        modelContent: 'item add',
+      };
+    case 'NO_VALUE':
+      return { ...state, showModal: true, modelContent: 'please enter value' };
+    case 'REMOVE':
+      return {
+        ...state,
+        people: state.people.filter((el) => el.id !== action.payload),
+      };
+    case 'CLOSE_MODAL':
+      return {
+        ...state,
+        showModal: false,
+      };
+
+    default:
+      return state;
+  }
+};
+
+const initialState = {
+  people: [],
+  showModal: false,
+  modelContent: '',
+};
 
 const Index = () => {
-  return <h2>useReducer</h2>;
+  const [text, setText] = useState('');
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const closeModal = () => {
+    dispatch({ type: 'CLOSE_MODAL' });
+  };
+  const submitHandler = (e) => {
+    e.preventDefault();
+    if (!text) {
+      dispatch({ type: 'NO_VALUE' });
+    } else {
+      const newItem = { id: uuidv4(), title: text };
+      dispatch({ type: 'ADD', payload: newItem });
+      setText('');
+    }
+  };
+
+  return (
+    <>
+      {state.showModal && <Modal closeModal={closeModal} modelContent={state.modelContent} />}
+      <form className="form" onSubmit={submitHandler}>
+        <div>
+          <input type="text" value={text} onChange={(e) => setText(e.target.value)} />
+        </div>
+
+        <button type="submit">add</button>
+      </form>
+      <article>
+        {state.people.map((el) => (
+          <div className="item" key={el.id}>
+            <h4>{el.title}</h4>
+            <button type="submit" onClick={() => dispatch({ type: 'REMOVE', payload: el.id })}>
+              remove
+            </button>
+          </div>
+        ))}
+      </article>
+    </>
+  );
 };
 
 export default Index;
