401bea1805c71356fc460d98d0be359505d3614e
Abderaouf Yettou <abderaouf.yettou@gmail.com>
2021-02-20 09:54:36 +0100

    custom hook

--- react-advanced-2020/src/tutorial/9-custom-hooks/setup/2-useFetch.js
+++ react-advanced-2020/src/tutorial/9-custom-hooks/setup/2-useFetch.js
@@ -1,3 +1,18 @@
 import { useState, useEffect } from 'react';
 
-export const useFetch = () => {};
+export const useFetch = (url) => {
+  const [loading, setLoading] = useState(true);
+  const [products, setProducts] = useState([]);
+
+  const getProducts = async () => {
+    const response = await fetch(url);
+    const data = await response.json();
+    setProducts(data);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getProducts();
+  }, [url]);
+  return { loading, products };
+};
