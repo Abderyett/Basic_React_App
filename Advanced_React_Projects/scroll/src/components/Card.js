52fd5648a40980b1c66bb61f8b30d564dbf32915
Abderaouf Yettou <abderaouf.yettou@gmail.com>
2021-03-22 19:23:06 +0100

    Fix Card's hover

--- Advanced_React_Projects/scroll/src/components/Card.js
+++ Advanced_React_Projects/scroll/src/components/Card.js
@@ -1,55 +1,35 @@
-import React, { useEffect, useRef } from 'react';
+import React from 'react';
 import styled from 'styled-components';
-import { useGlobalContext } from '../context';
 import { rounded, color } from '../utilities';
 
 function Card() {
-  const { showDetails, setShowDetails } = useGlobalContext();
-  const imageRef = useRef();
-  useEffect(() => {
-    const handler = (event) => {
-      if (!imageRef.current.contains(event.target)) {
-        setShowDetails(false);
-      }
-    };
-    document.addEventListener('mouseover', handler);
-    return () => {
-      document.removeEventListener('mouseover', handler);
-    };
-  });
   const url =
     'https://images.unsplash.com/photo-1612832164065-fc35ded2a291?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxNzY2ODF8MXwxfGFsbHwxfHx8fHx8Mnx8MTYxNjM5NjU3Ng&ixlib=rb-1.2.1&q=80&w=1080';
   const urlAuthor =
     'https://images.unsplash.com/profile-1587651800415-20eed2ec0209image?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&cs=tinysrgb&fit=crop&h=64&w=64';
   return (
-    <CardWrapper ref={imageRef} onMouseOver={() => setShowDetails(true)}>
-      <img src={url} alt="surface" />
-      {showDetails && (
-        <InfoWrapper>
-          <div>
-            <h4>Detail</h4>
-            <h5>Likes</h5>
-          </div>
+    <CardWrapper>
+      <Image src={url} alt="surface" />
 
-          <img src={urlAuthor} alt="author" />
+      <Article>
+        <InfoWrapper>
+          <h4>Detail</h4>
+          <h5>Likes</h5>
         </InfoWrapper>
-      )}
+
+        <InfoImage>
+          <img src={urlAuthor} alt="author" />
+        </InfoImage>
+      </Article>
     </CardWrapper>
   );
 }
 
 const CardWrapper = styled.div`
   position: relative;
-
-  img {
-    width: 40rem;
-    height: 25rem;
-    object-fit: cover;
-    border-radius: ${rounded.md};
-  }
 `;
 
-const InfoWrapper = styled.div`
+const Article = styled.article`
   background: rgba(0, 0, 0, 0.3);
   color: ${color.white};
   height: 5rem;
@@ -59,16 +39,31 @@ const InfoWrapper = styled.div`
   left: 0;
   border-bottom-left-radius: ${rounded.md};
   border-bottom-right-radius: ${rounded.md};
+  visibility: hidden;
   display: flex;
   justify-content: space-between;
   align-items: center;
-  div {
-    display: flex;
-    flex-direction: column;
-    margin-left: 2rem;
-    line-height: 1.5rem;
+  &:hover {
+    visibility: visible;
   }
+`;
+const Image = styled.img`
+  width: 40rem;
+  height: 25rem;
+  object-fit: cover;
+  border-radius: ${rounded.md};
+  &:hover ~ ${Article} {
+    visibility: visible;
+  }
+`;
+const InfoWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-left: 2rem;
+  line-height: 1.5rem;
+`;
 
+const InfoImage = styled.a`
   img {
     padding: 0.25rem;
     width: 3rem;
@@ -79,4 +74,5 @@ const InfoWrapper = styled.div`
     cursor: pointer;
   }
 `;
+
 export default Card;
--- Advanced_React_Projects/scroll/src/components/CradList.js
+++ Advanced_React_Projects/scroll/src/components/CradList.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import styled from 'styled-components';
 import Card from './Card';
 
 function CradList() {
   return (
-    <div>
+    <StyledCardList>
       <Card />
-    </div>
+      <Card />
+    </StyledCardList>
   );
 }
+
+const StyledCardList = styled.div`
+  display: grid;
+  justify-content: center;
+  grid-template-columns: repeat(auto-fit, 40rem);
+  grid-auto-rows: 25rem;
+  grid-gap: 2rem;
+`;
 
 export default CradList;
