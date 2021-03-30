48e77c4c8fe1e3e28a89c82f876a4242efb6f19e
Abderaouf Yettou <abderaouf.yettou@gmail.com>
2021-03-30 10:21:54 +0100

    Set heading insidegrid

--- Advanced_React_Projects/5-Movie_DB/src/components/CardList.js
+++ Advanced_React_Projects/5-Movie_DB/src/components/CardList.js
@@ -6,11 +6,10 @@ import Card from './Card';
 export function CardList() {
   return (
     <>
-      <Heading>
-        <h1>Popular Movies</h1>
-        <div />
-      </Heading>
       <CardContainer>
+        <Heading>
+          <h1>Popular Movies</h1>
+        </Heading>
         <Card />
         <Card />
         <Card />
@@ -24,26 +23,27 @@ export function CardList() {
 
 const CardContainer = styled.section`
   width: 95vw;
-  /* height: 100vh; */
+  max-width: 1400px;
+
   background-color: #bcccdc;
   margin: 0 auto;
   display: grid;
   grid-template-columns: repeat(auto-fit, 20rem);
+  grid-template-rows: 2rem;
   grid-auto-rows: 42rem;
   grid-gap: 2rem;
   justify-content: center;
+  @media screen and (min-width: 1400px) {
+    width: 85vw;
+    margin: 0 auto;
+  } ;
 `;
 const Heading = styled.div`
+  grid-column: 1/-1;
   h1 {
     padding-top: 2rem;
-    padding-left: 2.5rem;
+    padding-left: 0.25rem;
     color: ${color.black};
     font-size: 2.5rem;
-  }
-  div {
-    border-bottom: 3px solid ${color.black};
-    width: 19.5rem;
-    padding-top: 1rem;
-    margin-left: 3rem;
   }
 `;
