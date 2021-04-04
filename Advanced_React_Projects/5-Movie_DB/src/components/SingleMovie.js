8264d0bb69831f91176629c9af9b45e59dbc9176
Abderaouf Yettou <abderaouf.yettou@gmail.com>
2021-04-04 19:31:56 +0100

    display data and poster

--- Advanced_React_Projects/5-Movie_DB/src/components/SingleMovie.js
+++ Advanced_React_Projects/5-Movie_DB/src/components/SingleMovie.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import { link, useParams } from 'react-router-dom';
 import axios from 'axios';
 import styled from 'styled-components';
+import { format } from 'date-fns';
+import { color, rounded } from '../utilities';
 
 const imgUrl = 'https://image.tmdb.org/t/p/w1920_and_h800_multi_faces/';
 export function SingleMovie() {
@@ -23,22 +25,59 @@ export function SingleMovie() {
   useEffect(() => {
     fetchDetails(id);
   }, []);
-  const { backdrop_path: backdrop, genres, title, overview, vote_average: vote } = singleMovie;
+  const {
+    backdrop_path: backdrop,
+    genres,
+    title,
+    overview,
+    vote_average: vote,
+    poster_path: poster,
+    release_date: releaseDate,
+  } = singleMovie;
 
   return (
     <StyledContainer backdrop={backdrop}>
-      <DetailsContainer />
+      <ImageContainer>
+        <img src={`https://image.tmdb.org/t/p/w300_and_h450_bestv2${poster}`} alt={title} />
+      </ImageContainer>
+      <TextContainer>
+        <h1>{title}</h1>
+        <h2>
+          Genres :{/* {genres.map((el) => (
+            <span key={el.id}>{el.name.split(',')}</span>
+          ))} */}
+        </h2>
+      </TextContainer>
     </StyledContainer>
   );
 }
 
 const StyledContainer = styled.div`
   width: 100vw;
-  height: 50vh;
+  height: 483px;
   background: ${(props) => `url(https://image.tmdb.org/t/p/w1920_and_h800_multi_faces/${props.backdrop})`};
   background-repeat: no-repeat;
   background-size: cover;
-  filter: sepia(90%) hue-rotate(170deg) saturate(190%);
+  display: grid;
+  grid-template-columns: 30rem 40%;
+  grid-gap: 2rem;
+  padding-left: 30px;
+  grid-template-rows: auto;
+  justify-content: center;
+  align-content: center;
+
+  /* filter: sepia(90%) hue-rotate(170deg) saturate(190%); */
 `;
 
-const DetailsContainer = styled.div``;
+const ImageContainer = styled.div`
+  img {
+    border-radius: ${rounded.lg};
+    /* margin: 15px 0; */
+  }
+`;
+
+const TextContainer = styled.div`
+  color: ${color.blue_grey_800};
+
+  /* margin: 15px 0; */
+`;
