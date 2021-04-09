77702d51f861e110e7ac8d749792795f07754eba
Abderaouf Yettou <abderaouf.yettou@gmail.com>
2021-04-09 20:01:19 +0100

    Fix layout for mobile size on singlemovie

--- Advanced_React_Projects/5-Movie_DB/src/components/SingleMovie.js
+++ Advanced_React_Projects/5-Movie_DB/src/components/SingleMovie.js
@@ -90,18 +90,17 @@ export function SingleMovie() {
           </StyledGenre>
 
           <StatContainer>
-            <CircleSvg voteAverage={voteAverage} />
-
             <div>
-              User <br />
-              Score
+              <CircleSvg voteAverage={voteAverage} />
+
+              <div>User Score</div>
             </div>
             <button onClick={() => setShowModal(true)} type="submit">
               <span dataSi>&#9654; Play trailer</span>
             </button>
           </StatContainer>
-          <em>{tagline}</em>
           <Overview>
+            <h3>{tagline}</h3>
             <h2>Overview</h2> <br />
             <p> {overview}</p>
           </Overview>
@@ -132,20 +131,24 @@ const StyledContainer = styled.div`
   align-content: center;
   @media (max-width: 1024px) {
     grid-gap: 1rem;
-    grid-template-columns: auto 60%;
   }
   @media (max-width: 768px) {
     display: grid;
-    grid-template-columns: 350px;
+    grid-template-columns: 1fr;
     justify-content: center;
     align-content: center;
     padding-top: 1rem;
-    padding-left: 5px;
+    padding-left: 0;
     height: auto;
+    grid-gap: 12px;
+    background-repeat: no-repeat;
+    background-size: contain;
+    background-position: top center;
   }
 `;
 
 const ImageContainer = styled.div`
+  align-self: center;
   img {
     border-radius: ${rounded.lg};
     @media (max-width: 1024px) {
@@ -153,21 +156,25 @@ const ImageContainer = styled.div`
       height: 400px;
     }
     @media (max-width: 768px) {
-      width: 200px;
-      height: 330px;
+      width: 80px;
+      height: 130px;
+      margin-left: 2rem;
     }
   }
 `;
+
+// ? ============== Text Container ===============//
 
 const TextContainer = styled.div`
   color: ${color.white};
   font-family: 'Sans Regular';
   width: 100%;
-  max-width: 740px;
+  max-width: 768px;
   height: 450px;
   background: rgba(0, 0, 0, 0.7);
   padding: 2rem;
   border-radius: ${rounded.lg};
+
   display: flex;
   /* flex-wrap: wrap; */
   flex-direction: column;
@@ -178,42 +185,82 @@ const TextContainer = styled.div`
     padding: 1rem 2rem;
   }
   @media (max-width: 768px) {
-    padding: 1.5rem;
+    background-color: ${color.grey_800};
+    border-radius: unset;
+    justify-content: flex-start;
+    padding: 0;
+    flex-wrap: wrap;
   }
   h1 {
     font-size: 3.5rem;
     font-weight: 600;
     font-family: 'Sans Regular';
+
     @media (max-width: 1024px) {
       font-size: 2.5rem;
     }
     @media (max-width: 768px) {
       font-size: 2rem;
-    }
-  }
-  em {
-    color: ${color.grey_400};
-    font-size: 1.4rem;
-    font-family: 'Sans Regular';
-    @media (max-width: 768px) {
-      font-size: 0.5rem;
+      text-align: center;
+      padding: 1rem 2rem;
+      background-color: ${color.grey_900};
     }
   }
 `;
+// * ============== Genre Container ===============//
+
+const StyledGenre = styled.p`
+  font-size: 1.4rem;
+  padding: 2rem 1rem;
+  @media (max-width: 768px) {
+    text-align: center;
+    background-color: ${color.grey_900};
+    padding: 0;
+  }
+  &:after {
+    content: '  ●  ${(props) => props.runtime} m';
+  }
+
+  span:not(:empty) ~ span:not(:empty):before {
+    content: ',  ';
+  }
+  @media (max-width: 1024px) {
+    padding-bottom: 2rem;
+  }
+`;
+
+// ? ============== Stats Container ===============//
+
 const StatContainer = styled.div`
   display: flex;
   align-items: center;
   margin-bottom: 1rem;
   @media (max-width: 768px) {
     font-size: 0.5rem;
+    display: flex;
+    justify-content: space-around;
+    background-color: ${color.grey_900};
+    padding-bottom: 2rem;
+    div {
+      display: flex;
+      justify-content: center;
+      div {
+        padding: 1.5rem;
+        padding-left: 0.75rem;
+      }
+    }
   }
   div {
-    margin-left: 0.75rem;
+    display: flex;
+    justify-content: center;
     font-size: 1.5rem;
+    div {
+      padding: 1.5rem;
+      padding-left: 0.75rem;
+    }
   }
   button {
     margin-left: 1rem;
-
     cursor: pointer;
     color: white;
     background: transparent;
@@ -224,24 +271,52 @@ const StatContainer = styled.div`
     span {
       font-size: 1.5rem;
       font-family: 'Sans Regular';
+      padding: 1rem;
+
       @media (max-width: 768px) {
-        font-size: 1rem;
+        border-left: 1px solid ${color.grey_700};
+        padding-left: 5rem;
       }
     }
   }
 `;
+//! ============== Overview Container ===============//
+
 const Overview = styled.div`
-  margin-top: 2rem;
-  font-size: 1.5rem;
+  /* margin-top: 2rem; */
+
+  h2 {
+    padding-bottom: 1rem;
+  }
+  h3 {
+    color: ${color.grey_400};
+    font-size: 1.4rem;
+    letter-spacing: 1px;
+    font-style: italic;
+    padding-bottom: 1rem;
+    @media (max-width: 768px) {
+      background-color: ${color.grey_800};
+    }
+  }
   @media (max-width: 1024px) {
     font-size: 1.5rem;
   }
 
+  @media (max-width: 768px) {
+    padding: 1rem 2rem;
+    display: flex;
+    flex-direction: column;
+    flex-shrink: 1;
+    background-color: ${color.grey_800};
+  }
+
   p {
-    font-size: 1.25rem;
+    font-size: 1.5rem;
     line-height: 2rem;
   }
 `;
+
+//! ============== Cast and Movies Details ===============//
 const DetailsContainer = styled.section`
   display: flex;
   margin: 0 auto;
@@ -253,24 +328,5 @@ const DetailsContainer = styled.section`
     flex-direction: column;
     margin-top: 1rem;
     justify-content: left;
-  }
-`;
-
-const StyledGenre = styled.p`
-  font-size: 1.4rem;
-  padding-bottom: 4rem;
-  @media (max-width: 768px) {
-    font-size: 1rem;
-    padding-bottom: 2rem;
-  }
-  &:after {
-    content: '  ●  ${(props) => props.runtime}m';
-  }
-
-  span:not(:empty) ~ span:not(:empty):before {
-    content: ',  ';
-  }
-  @media (max-width: 1024px) {
-    padding-bottom: 2rem;
   }
 `;
